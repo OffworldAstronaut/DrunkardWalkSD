eef0b47d0a22c44196489c4062854fe8debc0ef9
OffworldAstronaut <rafael.science.amorim@gmail.com>
2025-05-31 13:19:09 -0300

    feat: initial structure and imports

---
+++ DrunkardWalkSD/main.py
@@ -0,0 +1,13 @@
+from time import time                       # For file R&W operations
+import numpy as np                          # General numerical necessities
+from matplotlib import pyplot as plt        # For plotting
+from typing import List                     # Typing hints
+
+class Drunkard: 
+    ...
+    
+class Sidewalk:
+    ...
+    
+class City:
+    ...
